5e581237dcaee6a7f1d95f704f03f2bbddea73ea
luicahleo <luisrolandocahuanaleon@gmail.com>
2021-09-20 17:31:51 +0200

    v2 js

--- resources/views/broker/index.blade.php
+++ resources/views/broker/index.blade.php
@@ -4,81 +4,63 @@
 <script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
 
 <script>
+    /*
+     ******************************
+     ****** CONEXION  *************
+     ******************************
+     */
 
+    // connect options
+    const options = {
+        connectTimeout: 4000,
 
-const options = {
-      connectTimeout: 4000,
+        // Authentication
+        clientId: 'iotmc',
+        username: 'web_client',
+        password: '121212',
 
-      clientId: 'asd',
-      keepalive: 60,
-      clean: true,
+        keepalive: 60,
+        clean: true,
     }
 
+    var connected = false;
+
+    // WebSocket connect url
     const WebSocket_URL = 'wss://trajano.es:8084/mqtt'
+
 
     const client = mqtt.connect(WebSocket_URL, options)
 
-    client.on('connect',() => {
-      console.log('Mqtt conectado por WS, Exito!!!')
 
-      client.subscribe('values', { qos:0 }, (error) => {
-        if (!error) {
-          console.log('Suscripcion exitosa');
-        }else {
-          console.log('Suscrpcion fallida');
-        }
-      })
+    client.on('connect', () => {
+        console.log('Mqtt conectado por WS! Exito!')
 
-      client.publish('fabrica', 'esto es un exito!!!', (error) => {
-        console.log(error || 'Mensaje enviado')
-      })
+        client.subscribe('values', {
+            qos: 0
+        }, (error) => {
+            if (!error) {
+                console.log('Suscripción exitosa!')
+            } else {
+                console.log('Suscripción fallida!')
+            }
+        })
+
+        // publish(topic, payload, options/callback)
+        client.publish('fabrica', 'esto es un verdadero éxito', (error) => {
+            console.log(error || 'Mensaje enviado!!!')
+        })
     })
 
     client.on('message', (topic, message) => {
-      console.log('Mensaje recibido bajo el topico: ', topic, ' -> ', message.toString());
+        console.log('Mensaje recibido bajo tópico: ', topic, ' -> ', message.toString())
+        process_msg(topic, message);
     })
 
-    client.on('reconnect',(error) => {
-      console.log('error al reconectar', error)
+    client.on('reconnect', (error) => {
+        console.log('Error al reconectar', error)
     })
 
-    client.on('error',(error) => {
-      console.log('error de conexion', error)
+    client.on('error', (error) => {
+        console.log('Error de conexión:', error)
     })
-
-
-
-    // Globally initializes an mqtt variable
-// console.log(mqtt)
-//
-// const clientId = 'mqttjs_a' //+ Math.random().toString(16).substr(2, 8)
-// const host = 'wss://bidon.trajano.tech:8094/mqtt'
-//
-// const options = {
-//   keepalive: 60,
-//   clientId: clientId,
-//   protocolId: 'MQTT',
-//   protocolVersion: 4,
-//   clean: true,
-//   reconnectPeriod: 1000,
-//   connectTimeout: 30 * 1000,
-//   will: {
-//     topic: 'WillMsg',
-//     payload: 'Connection Closed abnormally..!',
-//     qos: 0,
-//     retain: false
-//   },
-// }
-//
-// console.log('Connecting mqtt client')
-// const client = mqtt.connect(host, options)
-//
-// client.on('error', (err) => {
-//   console.log('Connection error: ', err)
-//   client.end()
-// })
-//
-// client.on('reconnect', () => {
-//   console.log('Reconnecting...')
-// })
 </script>
