31f78b371e8fab48807b9ea494d5826a1c7e005a
luicahleo <luisrolandocahuanaleon@gmail.com>
2021-09-20 15:45:02 +0200

    nueva ruta

---
+++ resources/views/broker/index.blade.php
@@ -0,0 +1,28 @@
+<h1>Broker index</h1>
+
+
+{{-- <script src="https://unpkg.com/mqtt@4.2.8/dist/mqtt.min.js">
+    const options = {
+        connectTimeout: 4000,
+
+        clientId: 'emqx',
+        keepalive: 60,
+        clean: true,
+    }
+
+    const WebSocket_URL = 'wss://trajano.es:8084/mqtt'
+
+    const client = mqtt.connect(WebSocket_URL, options)
+
+    client.on('connect', () => {
+        console.log('Mqtt conectado por WS, Exito!!!')
+    })
+
+    client.on('reconnect', (error) => {
+        console.log('error al reconectar', error)
+    })
+
+    client.on('error', (error) => {
+        console.log('error de conexion', error)
+    })
+</script> --}}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Broker\BrokerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('brokers', BrokerController::class)->names('brokers');
