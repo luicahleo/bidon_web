dd9040d1c80408f1bb97035b2af61edce106191b
luicahleo <luisrolandocahuanaleon@gmail.com>
2021-09-20 16:18:06 +0200

    nuevo mqtt

--- resources/views/broker/index.blade.php
+++ resources/views/broker/index.blade.php
@@ -1,28 +1,84 @@
 <h1>Broker index pruebas con pull nuevo</h1>
 
 
-<script src="https://unpkg.com/mqtt@4.2.8/dist/mqtt.min.js">
-    const options = {
-        connectTimeout: 4000,
+<script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
 
-        clientId: 'emqx',
-        keepalive: 60,
-        clean: true,
+<script>
+
+
+const options = {
+      connectTimeout: 4000,
+
+      clientId: 'asd',
+      keepalive: 60,
+      clean: true,
     }
 
     const WebSocket_URL = 'wss://trajano.es:8084/mqtt'
 
     const client = mqtt.connect(WebSocket_URL, options)
 
-    client.on('connect', () => {
-        console.log('Mqtt conectado por WS, Exito!!!')
+    client.on('connect',() => {
+      console.log('Mqtt conectado por WS, Exito!!!')
+
+      client.subscribe('commands', { qos:0 }, (error) => {
+        if (error) {
+          console.log('Suscripcion exitosa');
+        }else {
+          console.log('Suscrpcion fallida');
+        }
+      })
+
+      client.publish('fabrica', 'esto es un exito!!!', (error) => {
+        console.log(error || 'Mensaje enviado')
+      })
     })
 
-    client.on('reconnect', (error) => {
-        console.log('error al reconectar', error)
+    client.on('message', (topic, message) => {
+      console.log('Mensaje recibido bajo el topico: ', topic, ' -> ', message.toString());
     })
 
-    client.on('error', (error) => {
-        console.log('error de conexion', error)
+    client.on('reconnect',(error) => {
+      console.log('error al reconectar', error)
     })
+
+    client.on('error',(error) => {
+      console.log('error de conexion', error)
+    })
+
+
+
+    // Globally initializes an mqtt variable
+// console.log(mqtt)
+//
+// const clientId = 'mqttjs_a' //+ Math.random().toString(16).substr(2, 8)
+// const host = 'wss://bidon.trajano.tech:8094/mqtt'
+//
+// const options = {
+//   keepalive: 60,
+//   clientId: clientId,
+//   protocolId: 'MQTT',
+//   protocolVersion: 4,
+//   clean: true,
+//   reconnectPeriod: 1000,
+//   connectTimeout: 30 * 1000,
+//   will: {
+//     topic: 'WillMsg',
+//     payload: 'Connection Closed abnormally..!',
+//     qos: 0,
+//     retain: false
+//   },
+// }
+//
+// console.log('Connecting mqtt client')
+// const client = mqtt.connect(host, options)
+//
+// client.on('error', (err) => {
+//   console.log('Connection error: ', err)
+//   client.end()
+// })
+//
+// client.on('reconnect', () => {
+//   console.log('Reconnecting...')
+// })
 </script>
