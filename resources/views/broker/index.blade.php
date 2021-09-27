c3b3d5bdf445845c901a2cb3269faffae6cf2566
luicahleo <luisrolandocahuanaleon@gmail.com>
2021-09-27 18:03:19 +0200

    solucionado problema de mix

--- resources/views/broker/index.blade.php
+++ resources/views/broker/index.blade.php
@@ -17,7 +17,7 @@
                                 <div class="bg-indigo-500 rounded-lg p-2 xl:p-6">
                                     <h2 id="distance" class="title-font font-medium sm:text-4xl text-3xl text-white">
                                     </h2>
-                                    <i class="fa fa-water"></i>
+                                    <i class="fas fa-water"></i>
 
                                     <p class="leading-relaxed text-gray-100 font-bold">centimetros de nivel de agua</p>
                                 </div>
